bd2304af8a7d634117908d7ee0681efffc11d9d5
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-20 13:29:14 +0100

    app modified

---
+++ src/Component/Footer/menuData.js
@@ -0,0 +1,86 @@
+export const Links = [
+  {
+    title: 'COMPANY',
+    list: [
+      {
+        title: 'About Us',
+        slug: '/about-us',
+      },
+      {
+        title: 'Our Team',
+        slug: '/our-team',
+      },
+      {
+        title: 'Partners',
+        slug: '/partners',
+      },
+      {
+        title: 'Contact Us',
+        slug: '/contact-us',
+      },
+      {
+        title: 'Careers',
+        slug: '/careers',
+      },
+    ],
+  },
+  {
+    title: 'RESOURCES',
+    list: [
+      {
+        title: 'Coming Soon',
+        slug: '/coming-soon',
+      },
+      {
+        title: 'FAQs',
+        slug: '/FAQs',
+      },
+      {
+        title: 'Blog',
+        slug: '/blog',
+      },
+      {
+        title: 'Best Practices',
+        slug: '/best-practices',
+      },
+    ],
+  },
+  {
+    title: 'PRODUCT',
+    list: [
+      {
+        title: 'Features',
+        slug: '/features',
+      },
+      {
+        title: 'Integrations',
+        slug: '/integrations',
+      },
+      {
+        title: 'Prices',
+        slug: '/prices',
+      },
+      {
+        title: 'Demo',
+        slug: '/demo',
+      },
+    ],
+  },
+  {
+    title: 'COMMUNITY',
+    list: [
+      {
+        title: 'DevOps Community',
+        slug: '/devops-community',
+      },
+      {
+        title: 'News Room',
+        slug: '/news-room',
+      },
+      {
+        title: 'Podcasts',
+        slug: '/podcasts',
+      },
+    ],
+  },
+];
